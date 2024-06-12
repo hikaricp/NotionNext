1ced590f09e81032c77a4865d878f2144f27aef3
hikaricp <76799331+hikaricp@users.noreply.github.com>
2024-06-12 19:44:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,9 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [],
+  HEO_NOTICE_BAR: [
+    { title: '欢迎来到我的博客', url: '' },
+  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
